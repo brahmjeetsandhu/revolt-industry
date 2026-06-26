edd8b136a6c55c8c84f2aff760e1780b3797489e
FIRST_NAME LAST_NAME <research.brahmjeet.sandhu@gmail.com>
2026-06-26 10:48:30 +0100

    Intro section updated for new text

--- src/app/components/Introduction.jsx
+++ src/app/components/Introduction.jsx
@@ -1,14 +1,20 @@
 
 import BgImage from "../../assets/Intro-bg.png";
 export default function Introduction() {
-  return (  
-<section className="introduction apple-reveal" style={{ backgroundImage: `url(${BgImage.src})`,  backgroundSize: "cover",
-        backgroundPosition: "center", height: "100vh" }}>
-    <div className="introduction apple-reveal delay-4">
-        <h1>Who is Revolt Industry for</h1>
-        <p>Revolt Industry is a publishing platform built to support the next generation of manga, webcomics, and comics creators.
-Currently, there is an apparent shortage of creative and exciting stories. 
-</p>
-    </div>
-</section>  
-);};
+    return (
+        <section className="introduction apple-reveal" style={{
+            backgroundImage: `url(${BgImage.src})`, backgroundSize: "cover",
+            backgroundPosition: "center", height: "100vh"
+        }}>
+            <div className="introduction apple-reveal delay-4">
+                <h1>Who is Revolt Industry for</h1>
+                <p> Revolt Industry is a next-generation publishing and entertainment platform built to discover, develop,
+                    and connect the next generation of storytellers with audiences worldwide.
+                    Focused on manga-inspired storytelling, webcomics, light novels, and comics,
+                    Revolt creates a new ecosystem where creators can build original worlds, readers can discover new experiences,
+                    and future entertainment franchises can be born.
+                </p>
+            </div>
+        </section>
+    );
+};
